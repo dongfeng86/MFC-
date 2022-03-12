3a10e9d9ba7ada311d1c2d658df535cedaa156fd
dongfeng86 <115949168@qq.com>
2022-03-12 20:44:31 +0800

    20220312 家中

--- CustomComboBox/CustomComboBoxDlg.cpp
+++ CustomComboBox/CustomComboBoxDlg.cpp
@@ -13,6 +13,206 @@
 #endif
 
 
+// CSWColorComboBox
+IMPLEMENT_DYNAMIC(CSWColorComboBox, CComboBox)
+CSWColorComboBox::TColorProperty CSWColorComboBox::s_clrPropertys[MAX_COLOR_COUNT] =
+{
+	TColorProperty(RGB(0x00, 0x00, 0x00), _T("黑色(Black)")),
+	TColorProperty(RGB(0x80, 0x00, 0x00), _T("褐红色(Maroon)")),
+	TColorProperty(RGB(0x00, 0x80, 0x00), _T("调和绿(Green)")),
+	TColorProperty(RGB(0x80, 0x80, 0x00), _T("橄榄色(Olive)")),
+	TColorProperty(RGB(0x00, 0x00, 0x80), _T("藏青色(Navy)")),
+	TColorProperty(RGB(0x80, 0x00, 0x80), _T("紫色(Purple)")),
+	TColorProperty(RGB(0x00, 0x80, 0x80), _T("鸭翅绿(Teal)")),
+	TColorProperty(RGB(0x80, 0x80, 0x80), _T("灰色(Gray)")),
+	TColorProperty(RGB(0xC0, 0xC0, 0xC0), _T("银色(Silver)")),
+	TColorProperty(RGB(0xFF, 0x00, 0x00), _T("红色(Red)")),
+	TColorProperty(RGB(0x00, 0xFF, 0x00),_T("绿色(Lime)")),
+	TColorProperty(RGB(0xFF, 0xFF, 0x00), _T("黄色(Yellow)")),
+	TColorProperty(RGB(0x00, 0x00, 0xFF), _T("蓝色(Blue)")),
+	TColorProperty(RGB(0xFF, 0x00, 0xFF), _T("紫红色(Fushcia)")),
+	TColorProperty(RGB(0x00, 0xFF, 0xFF),_T("青色(Aqua)")),
+	TColorProperty(RGB(0xFF, 0xFF, 0xFF), _T("白色(White)")),
+};
+CSWColorComboBox::CSWColorComboBox()
+{
+}
+
+CSWColorComboBox::~CSWColorComboBox()
+{
+}
+
+BEGIN_MESSAGE_MAP(CSWColorComboBox, CComboBox)
+	ON_WM_CREATE()
+END_MESSAGE_MAP()
+
+
+void CSWColorComboBox::CommonConstruct(void)
+{
+	for (int nIndex = 0; nIndex < MAX_COLOR_COUNT; nIndex++)
+	{
+		int nItem = AddString(s_clrPropertys[nIndex].m_szName);
+		if (nItem == CB_ERRSPACE)
+			break;
+		else
+			SetItemData(nItem, s_clrPropertys[nItem].m_clrColor);
+	}
+}
+
+void CSWColorComboBox::DrawItem(LPDRAWITEMSTRUCT pDIStruct)
+{
+	CDC dc;
+	if (!dc.Attach(pDIStruct->hDC))
+		return;
+
+	CString strName;
+	CRect rItemRect(pDIStruct->rcItem);
+	CRect rcFrame(rItemRect);
+	CRect rcText(rcFrame);
+
+	// 颜色边框画刷
+	CBrush brFrameBrush;
+	brFrameBrush.CreateStockObject(BLACK_BRUSH);
+
+	int nFrameWidth = 0;
+	int nItem = pDIStruct->itemID;
+	int nAction = pDIStruct->itemAction;
+	int nState = pDIStruct->itemState;
+	COLORREF clrColor = 0;
+	COLORREF clrNormal = GetSysColor(COLOR_WINDOW);
+	COLORREF clrSelected = GetSysColor(COLOR_HIGHLIGHT);
+	COLORREF clrText = GetSysColor(COLOR_WINDOWTEXT);
+
+	// 颜色框宽度
+	nFrameWidth = 40;
+	if (nState & ODS_SELECTED)
+	{
+		dc.SetTextColor((0x00FFFFFF & ~(clrText)));
+		dc.SetBkColor(clrSelected);
+		dc.FillSolidRect(&rcFrame, clrSelected);
+	}
+	else
+	{
+		dc.SetTextColor(clrText);
+		dc.SetBkColor(clrNormal);
+		if (pDIStruct->itemAction & ODA_DRAWENTIRE
+			&& pDIStruct->itemState & ODS_COMBOBOXEDIT)
+			dc.FillSolidRect(&rcFrame, ::GetSysColor(COLOR_BTNSHADOW));
+		else
+			dc.FillSolidRect(&rcFrame, clrNormal);
+	}
+
+	if (nState & ODS_FOCUS)
+		dc.DrawFocusRect(&rItemRect);
+
+	rcFrame.left += 2;
+	rcFrame.right = nFrameWidth;
+	rcFrame.top += 2;
+	rcText.left += (nFrameWidth + 2);
+	rcText.DeflateRect(CSize(2, 2));
+
+	if (nItem != -1)
+	{
+		GetLBText(nItem, strName);
+		if (nState & ODS_DISABLED)
+		{
+			clrColor = GetSysColor(COLOR_INACTIVECAPTIONTEXT);
+			dc.SetTextColor(clrColor);
+		}
+		else
+		{
+			clrColor = GetItemData(nItem);
+		}
+
+		dc.SetBkMode(TRANSPARENT);
+		dc.TextOut(rcText.left, rcText.top, strName);
+		if (clrColor != RGB(0, 0, 1))
+		{
+			dc.FillSolidRect(&rcFrame, clrColor);
+			dc.FrameRect(&rcFrame, &brFrameBrush);
+		}
+	}
+
+	dc.Detach();
+}
+
+COLORREF CSWColorComboBox::GetSelColor()
+{
+	int nSelIndex = GetCurSel();
+	if (nSelIndex == CB_ERR)
+		return RGB(0, 0, 0);
+
+	return GetItemData(nSelIndex);
+}
+
+void CSWColorComboBox::SelectColor(COLORREF color)
+{
+	int nCount = GetCount();
+	for (int nIndex = 0; nIndex < nCount; nIndex++)
+	{
+		if (color == GetItemData(nIndex))
+			SetCurSel(nIndex);
+	}
+}
+
+void CSWColorComboBox::SelectColor(LPCTSTR lpszName)
+{
+	int nCount = GetCount();
+	CString sColorName;
+	for (int nIndex = 0; nIndex < nCount; nIndex++)
+	{
+		GetLBText(nIndex, sColorName);
+		if (!sColorName.CompareNoCase(lpszName))
+			SetCurSel(nIndex);
+	}
+}
+
+void CSWColorComboBox::DeleteColor(LPCTSTR lpszName)
+{
+	int nCount = GetCount();
+	CString strColorName;
+	for (int nIndex = 0; nIndex < nCount; nIndex++)
+	{
+		GetLBText(nIndex, strColorName);
+		if (!strColorName.CompareNoCase(lpszName))
+		{
+			DeleteString(nIndex);
+			break;
+		}
+	}
+}
+
+void CSWColorComboBox::DeleteColor(COLORREF color)
+{
+	int nCount = GetCount();
+	for (int nIndex = 0; nIndex < nCount; nIndex++)
+	{
+		if (color == GetItemData(nIndex))
+		{
+			DeleteString(nIndex);
+			break;
+		}
+	}
+}
+
+int CSWColorComboBox::AddColor(LPCTSTR lpszName, COLORREF color)
+{
+	int nIndex = InsertString(-1, lpszName);
+	if (nIndex != CB_ERR)
+		SetItemData(nIndex, color);
+
+	return nIndex;
+}
+
+void CSWColorComboBox::PreSubclassWindow()
+{
+	// TODO: 在此添加专用代码和/或调用基类
+	CComboBox::PreSubclassWindow();
+	CommonConstruct();
+	SetCurSel(0);
+}
+
+
 // 用于应用程序“关于”菜单项的 CAboutDlg 对话框
 
 class CAboutDlg : public CDialogEx
@@ -60,6 +260,8 @@ void CCustomComboBoxDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
 	DDX_Control(pDX, IDC_COMBO1, m_wndCmbCustom);
+	//DDX_Control(pDX, IDC_COMBO2, m_wndComboNorml);
+	//DDX_Control(pDX, IDC_CMB_EXAMPLE, m_wndCmbNew);
 }
 
 BEGIN_MESSAGE_MAP(CCustomComboBoxDlg, CDialog)
@@ -68,6 +270,7 @@ BEGIN_MESSAGE_MAP(CCustomComboBoxDlg, CDialog)
 	ON_WM_QUERYDRAGICON()
 	ON_CBN_KILLFOCUS(IDC_COMBO1, &CCustomComboBoxDlg::OnCbnKillfocusCombo1)
 	ON_CBN_SELCHANGE(IDC_COMBO1, &CCustomComboBoxDlg::OnCbnSelchangeCombo1)
+	ON_WM_DRAWITEM()
 END_MESSAGE_MAP()
 
 
@@ -103,12 +306,11 @@ BOOL CCustomComboBoxDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
 	// TODO: 在此添加额外的初始化代码
-	int iiiii;
-	iiiii = m_wndCmbCustom.InsertString(0, _T("hello"));
-	iiiii = m_wndCmbCustom.InsertString(1, _T("he"));
-	iiiii = ((CComboBox*)GetDlgItem(IDC_COMBO2))->GetItemHeight(-1);
-	//iiiii= ((CComboBox*)GetDlgItem(IDC_COMBO1))->GetItemHeight(-1);
-	((CComboBox*)GetDlgItem(IDC_COMBO1))->SetItemHeight(-1, iiiii);
+	m_wndCmbCustom.InsertString(0, _T("hello"));
+	m_wndCmbCustom.InsertString(1, _T("he"));
+	m_wndCmbCustom.InsertString(1, _T("h"));
+
+
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
 
@@ -125,9 +327,6 @@ void CCustomComboBoxDlg::OnSysCommand(UINT nID, LPARAM lParam)
 	}
 }
 
-// 如果向对话框添加最小化按钮，则需要下面的代码
-//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
-//  这将由框架自动完成。
 
 void CCustomComboBoxDlg::OnPaint()
 {
@@ -161,9 +360,7 @@ HCURSOR CCustomComboBoxDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
-
 IMPLEMENT_DYNAMIC(CCustomComboBox, CComboBox)
-
 
 CCustomComboBox::CCustomComboBox()
 	:CComboBox()
@@ -174,54 +371,78 @@ CCustomComboBox::~CCustomComboBox()
 
 void CCustomComboBox::DrawItem(LPDRAWITEMSTRUCT lpDrawItemStruct)
 {
-	
-	LPCTSTR lpszText = (LPCTSTR)lpDrawItemStruct->itemData;
-	CString sItem;
-	GetLBText(lpDrawItemStruct->itemID,sItem);
-	if (!sItem.IsEmpty())
-	{
-		int iCount = GetCount();	//判断这个列表框一共有多少项
+	CDC dc;
+	dc.Attach(lpDrawItemStruct->hDC);
+	COLORREF crOldTextColor = dc.GetTextColor();	//完成绘图后恢复DC里的值
+	COLORREF crOldBkColor = dc.GetBkColor();
 
-		CDC dc;
-		dc.Attach(lpDrawItemStruct->hDC);
-		COLORREF crOldTextColor = dc.GetTextColor();	//完成绘图后恢复DC里的值
-		COLORREF crOldBkColor = dc.GetBkColor();
-		//处理最后一个项目，设置其背景和文字
-		if ((lpDrawItemStruct->itemAction & ODA_SELECT) &&
-			(lpDrawItemStruct->itemState  & ODS_SELECTED))
-		{
-			dc.SetTextColor(::GetSysColor(COLOR_HIGHLIGHTTEXT));
-			dc.SetBkColor(::GetSysColor(COLOR_HIGHLIGHT));
-			dc.FillSolidRect(&lpDrawItemStruct->rcItem, ::GetSysColor(COLOR_HIGHLIGHT));
-		}
+	// 选中一项，包括重绘编辑框和下拉列表某一项
+	if (lpDrawItemStruct->itemState  & ODS_SELECTED)
+	{
+		dc.SetTextColor(::GetSysColor(COLOR_HIGHLIGHTTEXT));
+		dc.SetBkColor(::GetSysColor(COLOR_HIGHLIGHT));
+		dc.FillSolidRect(&lpDrawItemStruct->rcItem, ::GetSysColor(COLOR_HIGHLIGHT));
+	}
+	// 控件获取焦点
+	else if (lpDrawItemStruct->itemState  & ODS_FOCUS)
+	{
+		dc.SetTextColor(::GetSysColor(COLOR_HIGHLIGHTTEXT));
+		dc.SetBkColor(::GetSysColor(COLOR_HIGHLIGHT));
+		dc.FillSolidRect(&lpDrawItemStruct->rcItem, ::GetSysColor(COLOR_HIGHLIGHT));
+	}
+	else
+	{
+		//绘制常态下的编辑框
+		if (lpDrawItemStruct->itemAction & ODA_DRAWENTIRE &&
+			lpDrawItemStruct->itemState & ODS_COMBOBOXEDIT)
+			dc.FillSolidRect(&lpDrawItemStruct->rcItem, ::GetSysColor(COLOR_BTNSHADOW));
+		//绘制常态下的下拉列表
 		else
 		{
-			if (iCount && lpDrawItemStruct->itemID == iCount - 1)
+			int iCount = GetCount();
+			if (iCount && lpDrawItemStruct->itemID == iCount - 1)	//绘制最后一项
 			{
-				dc.FillSolidRect(&lpDrawItemStruct->rcItem, ::GetSysColor(COLOR_3DFACE));	//获取对话框背景颜色
-				dc.DrawEdge(&lpDrawItemStruct->rcItem, EDGE_BUMP, BF_RECT);
+				CRect rect = lpDrawItemStruct->rcItem;
+				//dc.FillSolidRect(&lpDrawItemStruct->rcItem, ::GetSysColor(COLOR_3DFACE));	//获取对话框背景颜色
+				dc.FillSolidRect(&lpDrawItemStruct->rcItem, RGB(0,255,0));	//为了表示清楚，此处显示红色
+				dc.DrawEdge(&lpDrawItemStruct->rcItem, EDGE_BUMP, BF_RECT);	//绘制一个矩形框
 			}
-			else
+			else   //绘制除最后一项的其他项
 				dc.FillSolidRect(&lpDrawItemStruct->rcItem, crOldBkColor);
 		}
+	}
 
+	CString sItem;
+	//最后一项被选中后不在编辑框中绘制文本
+	if (lpDrawItemStruct->itemID != -1 &&
+		!(lpDrawItemStruct->itemID == GetCount() - 1 && lpDrawItemStruct->itemState & ODS_COMBOBOXEDIT))
+	{
+		GetLBText(lpDrawItemStruct->itemID, sItem);
 		dc.DrawText(sItem, sItem.GetLength(), &lpDrawItemStruct->rcItem,
 			DT_LEFT | DT_SINGLELINE | DT_VCENTER);
-		dc.SetTextColor(crOldTextColor);
-		dc.SetBkColor(crOldBkColor);
-		dc.Detach();
 	}
+
+	dc.SetTextColor(crOldTextColor);
+	dc.SetBkColor(crOldBkColor);
+	dc.Detach();
 }
+
 
 void CCustomComboBoxDlg::OnCbnKillfocusCombo1()
 {
 	// TODO: 在此添加控件通知处理程序代码
-	CString str;
-	m_wndCmbCustom.GetWindowText(str);
-	m_wndCmbCustom.InsertString(0, str);
+
 }
 
 void CCustomComboBoxDlg::OnCbnSelchangeCombo1()
 {
 	// TODO: 在此添加控件通知处理程序代码
+}
+
+
+void CCustomComboBoxDlg::OnDrawItem(int nIDCtl, LPDRAWITEMSTRUCT lpDrawItemStruct)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CDialog::OnDrawItem(nIDCtl, lpDrawItemStruct);
 }
--- CustomComboBox/CustomComboBoxDlg.h
+++ CustomComboBox/CustomComboBoxDlg.h
@@ -3,6 +3,7 @@
 //
 
 #pragma once
+#include <string.h>
 
 class CCustomComboBox :public CComboBox
 {
@@ -14,6 +15,63 @@ public:
 
 //protected:
 	virtual void DrawItem(LPDRAWITEMSTRUCT lpDrawItemStruct);
+};
+
+
+class CSWColorComboBox : public CComboBox
+{
+	DECLARE_DYNAMIC(CSWColorComboBox)
+
+#define MAX_COLOR_COUNT 16
+#define MAX_COLOR_NAME	16
+	struct TColorProperty
+	{
+		COLORREF m_clrColor;
+		TCHAR m_szName[MAX_COLOR_NAME];
+		TColorProperty()
+		{
+			memset(this, 0, sizeof(TColorProperty));
+		};
+		TColorProperty(COLORREF color, LPCTSTR lpszName)
+			: TColorProperty()
+		{
+			m_clrColor = color;								// 颜色值
+			_tcscpy_s(m_szName, lpszName);
+			//strncpy_s(m_szName, lpszName, MAX_COLOR_NAME);	// 颜色名称
+		};
+	};
+
+public:
+	CSWColorComboBox();
+	virtual ~CSWColorComboBox();
+
+private:
+	static TColorProperty s_clrPropertys[MAX_COLOR_COUNT]; // WEB标准颜色数组
+
+private:
+	void CommonConstruct(); // 初始化
+
+protected:
+	virtual void DrawItem(LPDRAWITEMSTRUCT lpDrawItemStruct);
+	virtual void PreSubclassWindow();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+
+public:
+	// 获取选择色彩值
+	COLORREF GetSelColor();
+
+	// 选择色彩项
+	void SelectColor(COLORREF color);
+	void SelectColor(LPCTSTR lpszName);
+
+	// 删除色彩项
+	void DeleteColor(LPCTSTR lpszName);
+	void DeleteColor(COLORREF color);
+
+	// 添加色彩项
+	int AddColor(LPCTSTR lpszName, COLORREF color);
 };
 
 
@@ -44,4 +102,9 @@ private:
 public:
 	afx_msg void OnCbnKillfocusCombo1();
 	afx_msg void OnCbnSelchangeCombo1();
+private:
+	//CComboBox m_wndComboNorml;
+	//CSWColorComboBox m_wndCmbNew;
+public:
+	afx_msg void OnDrawItem(int nIDCtl, LPDRAWITEMSTRUCT lpDrawItemStruct);
 };
