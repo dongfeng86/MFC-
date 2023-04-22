4785874a6d8bcc7c9df522f861965fbb902e9404
dongfeng86 <115949168@qq.com>
2023-04-22 22:08:17 +0800

    2023.4.22

--- DLL2(未采用Def文件)/Dll2.h
+++ DLL2(未采用Def文件)/Dll2.h
@@ -4,6 +4,18 @@
 #define DLL_API extern "C" _declspec(dllimport)
 #endif
 
-DLL_API int Add(int a,int b);
+DLL_API int add(int a,int b);
 
-DLL_API int SubTract(int a,int b);
+DLL_API int subtract(int a,int b);
+
+class _declspec(dllexport) Point
+{
+public:
+	Point();
+	~Point();
+	void OutPut(int x, int y);
+private:
+	double m_dXPoint;
+	double m_dYPoint;
+};
+
